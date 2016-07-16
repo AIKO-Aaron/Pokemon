d9496a50ec16eb4ef45cfffd84d4845f7f8b439b
Aaron Hodel <aaron@Aarons-MacBook-Pro.local>
2016-07-16 16:00:17 +0200

    ...

--- src/ch/aiko/pokemon/graphics/menu/MainMenu.java
+++ src/ch/aiko/pokemon/graphics/menu/MainMenu.java
@@ -4,9 +4,9 @@ import ch.aiko.engine.graphics.Renderer;
 import ch.aiko.engine.graphics.Screen;
 
 public class MainMenu extends Menu {
-	
+
 	final int width = 250;
-	
+
 	public MainMenu(Screen parent) {
 		super(parent);
 	}
@@ -22,10 +22,10 @@ public class MainMenu extends Menu {
 	public String getName() {
 		return "MainMenu";
 	}
-	
+
 	public void render(Renderer r) {
-		r.drawRect(r.getWidth()-width, 0, width, r.getHeight(), 0xFF000000, 5);
-		r.fillRect(r.getWidth()-width+5, 5, width-10, r.getHeight()-10, 0xFFFFFFFF);
+		r.drawRect(r.getWidth() - width, 0, width, r.getHeight(), 0xFF000000, 5);
+		r.fillRect(r.getWidth() - width + 5, 5, width - 10, r.getHeight() - 10, 0xFFFFFFFF);
 	}
 
 	public void onOpen() {}
