04ef8dd9b1f923a7e1ff75e0ac4c610ea138b97c
aaronhodel@gmail.com <aaronhodel@gmail.com>
2016-08-01 15:51:17 +0200

    Kinda working

--- push.sh
+++ push.sh
@@ -1,7 +1,10 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp ~/Dropbox/Public/Pokemon.jar ./builds/newest.jar
+echo "Commit branch:"
+read branch
+git checkout "$branch"
 echo "Commit message:"
 git add .
 read commit
 git commit -m "$commit"
-git push -u origin master
+git push -u origin "$branch"
